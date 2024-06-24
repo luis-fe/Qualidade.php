284cfd0c5503ff95322089bdf7229c71f305f6f5
Luis Fernando Gonçalves <80552965+luis-fe@users.noreply.github.com>
2024-06-24 07:59:27 -0300

    Update index.php

--- Wms/src/Fila Reposicao/index.php
+++ Wms/src/Fila Reposicao/index.php
@@ -116,6 +116,39 @@ include_once("../../../templates/Loading.php")
     </div>
 </div>
 
+<div class="modal fade" id="dataModal2" tabindex="-1" aria-labelledby="dataModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-xl">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="dataModalLabel2"></h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <div class="table-responsive">
+                    <table class="table table-bordered table-striped" id="dataTable2">
+                        <thead id="fixed-header">
+                            <tr>
+                                <th>Data Entrada</th>
+                                <th>Cod Barras Tag</th>
+                                <th>Cod Reduzido</th>
+                                <th>EPC</th>
+                                <th>Numero Op</th>
+
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <!-- Data will be appended here -->
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fechar</button>
+            </div>
+        </div>
+    </div>
+</div>
+
 
 <?php include_once("../../../templates/footer.php"); ?>
 <script src="script.js"></script>
