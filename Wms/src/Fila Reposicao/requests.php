c94caafcd326b4688c3e1a4c2a1914c6d013193e
Luis Fernando Gonçalves <80552965+luis-fe@users.noreply.github.com>
2024-07-04 16:13:39 -0300

    Update requests.php

--- Wms/src/Fila Reposicao/requests.php
+++ Wms/src/Fila Reposicao/requests.php
@@ -9,10 +9,11 @@ if (isset($_SESSION['usuario']) && isset($_SESSION['empresa'])) {
     $empresa = $_SESSION['empresa'];
     $token = $_SESSION['token'];
 } else {
-    header("Location: ../../../index_.php");
+    header("Location: ../../../index.php");
 }
 
-function ConsultarFila($empresa, $token, $natureza) {
+function ConsultarFila($empresa, $token, $natureza)
+{
     $baseUrl = ($empresa == "1") ? 'http://192.168.0.183:5000' : 'http://192.168.0.184:5000';
     $apiUrl = "{$baseUrl}/api/DetalhamentoFila?empresa={$empresa}&natureza={$natureza}";
     $ch = curl_init($apiUrl);
@@ -33,7 +34,30 @@ function ConsultarFila($empresa, $token, $natureza) {
     return json_decode($apiResponse, true);
 }
 
-function ConsultarCaixa($empresa, $token, $numCaixa) {
+function AtualizarFila($empresa, $token)
+{
+    $baseUrl = ($empresa == "1") ? 'http://192.168.0.183:5000' : 'http://192.168.0.184:5000';
+    $apiUrl = "{$baseUrl}/api/AtualizarTagsEstoque?empresa={$empresa}&IntervaloAutomacao=20";
+    $ch = curl_init($apiUrl);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, [
+        'Content-Type: application/json',
+        "Authorization: {$token}",
+    ]);
+
+    $apiResponse = curl_exec($ch);
+
+    if (!$apiResponse) {
+        error_log("Erro na requisição: " . curl_error($ch), 0);
+    }
+
+    curl_close($ch);
+
+    return json_decode($apiResponse, true);
+}
+
+function ConsultarCaixa($empresa, $token, $numCaixa)
+{
     $baseUrl = ($empresa == "1") ? 'http://192.168.0.183:5000' : 'http://192.168.0.184:5000';
     $apiUrl = "{$baseUrl}/api/DetalharCaixa?numeroCaixa={$numCaixa}";
     $ch = curl_init($apiUrl);
@@ -54,7 +78,8 @@ function ConsultarCaixa($empresa, $token, $numCaixa) {
     return json_decode($apiResponse, true);
 }
 
-function ConsultarReduzidos($empresa, $natureza, $token, $numeroOp, $codReduzido) {
+function ConsultarReduzidos($empresa, $natureza, $token, $numeroOp, $codReduzido)
+{
     $baseUrl = ($empresa == "1") ? 'http://192.168.0.183:5000' : 'http://192.168.0.184:5000';
     $apiUrl = "{$baseUrl}/api/DetalhaTagsNumeroOPReduzido?numeroop={$numeroOp}&codreduzido={$codReduzido}&codEmpresa={$empresa}&natureza={$natureza}";
     $ch = curl_init($apiUrl);
@@ -79,24 +104,25 @@ if ($_SERVER["REQUEST_METHOD"] == "GET") {
     if (isset($_GET["acao"])) {
         $acao = $_GET["acao"];
 
-        
+
 
         if ($acao == 'Consultar_Fila') {
             $natureza = isset($_GET["natureza"]) ? $_GET["natureza"] : "";
             header('Content-Type: application/json');
             echo json_encode(ConsultarFila($empresa, $token, $natureza));
-        } elseif($acao == 'Consultar_Caixa'){
+        } elseif ($acao == 'Consultar_Caixa') {
             $numCaixa = isset($_GET["numCaixa"]) ? $_GET["numCaixa"] : "";
             header('Content-Type: application/json');
             echo json_encode(ConsultarCaixa($empresa, $token, $numCaixa));
-
-        } elseif($acao == 'Consultar_Reduzido'){
+        } elseif ($acao == 'Consultar_Reduzido') {
             $natureza = isset($_GET["natureza"]) ? $_GET["natureza"] : "";
             $codReduzido = isset($_GET["codReduzido"]) ? $_GET["codReduzido"] : "";
             $numeroOp = isset($_GET["numeroOp"]) ? $_GET["numeroOp"] : "";
             header('Content-Type: application/json');
             echo json_encode(ConsultarReduzidos($empresa, $natureza, $token, $numeroOp, $codReduzido));
-
+        } elseif ($acao == 'Atualizar_Fila') {
+            header('Content-Type: application/json');
+            echo json_encode(AtualizarFila($empresa, $token));
         }
     }
 } else {
@@ -104,4 +130,3 @@ if ($_SERVER["REQUEST_METHOD"] == "GET") {
     header('Content-Type: application/json');
     echo json_encode(['status' => false, 'message' => 'Erro: Método de requisição não suportado.']);
 }
-?>
