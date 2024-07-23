54966c65be5ce0e137aebefd7c4b9f697a31e79f
Luis Fernando Gonçalves <80552965+luis-fe@users.noreply.github.com>
2024-07-23 17:23:20 -0300

    Update index.php

--- Gestao/FilaDeFases/index.php
+++ Gestao/FilaDeFases/index.php
@@ -71,6 +71,6 @@ include_once("../../templates/loading1.php");
     </div>
 </div>
 
-<?php include_once("../../templates/footer.php"); ?>
+<?php include_once("../../templates/footer1.php"); ?>
 
 <script src="https://cdn.jsdelivr.net/npm/apexcharts"></script>
