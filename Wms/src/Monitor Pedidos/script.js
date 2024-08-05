de36e70e01665797c94cdb95dfbc9254aaa84199
Luis Fernando Gonçalves <80552965+luis-fe@users.noreply.github.com>
2024-08-05 08:33:51 -0300

    Update script.js

--- Wms/src/Monitor Pedidos/script.js
+++ Wms/src/Monitor Pedidos/script.js
@@ -41,6 +41,7 @@ let PriorizacaoSelecionado = "";
         const dataFormatada = getdataFormatada(dataAtual);
         $('#data-inicio-pedido').val(dataFormatada);
         $('#data-fim-pedido').val(dataFormatada);
+        $('#data-emissao-inicial').val(dataFormatada);
         $('#data-inicio-ops').val(dataFormatada);
         $('#data-fim-ops').val(dataFormatada);
 
