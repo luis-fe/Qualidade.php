49384cc6442c1e8f6d964fb33f936e3881601260
Luis Fernando Gonçalves <80552965+luis-fe@users.noreply.github.com>
2024-09-05 17:41:48 -0300

    Update script.js

--- Gestao/MetasTerceirizados/script.js
+++ Gestao/MetasTerceirizados/script.js
@@ -1,3 +1,4 @@
+let MensagemModal
 const Consulta_Planos_Disponiveis = async () => {
     $('#loadingModal').modal('show');
     try {
@@ -37,21 +38,21 @@ function Tabela_Planos(listaPlanos) {
         pageLength: 10,
         data: listaPlanos,
         columns: [{
-                data: '01- Codigo Plano'
-            },
-            {
-                data: '02- Descricao do Plano'
-            },
-            {
-                data: null,
-                render: function(data, type, row) {
-                    return `
+            data: '01- Codigo Plano'
+        },
+        {
+            data: '02- Descricao do Plano'
+        },
+        {
+            data: null,
+            render: function (data, type, row) {
+                return `
                     <div" style="display: flex; justify-content: space-around; align-items: center; height: 100%;">
                         <button class="btn" style="background-color: var(--CorMenu); color: var(--Branco);" onclick="$('#codigoPlano').val('${row['01- Codigo Plano']}'); ConsultaLote(); $('#ModalPlanosDisponiveis').modal('hide')">Selecionar</button>
                     </div>
                 `;
-                }
             }
+        }
         ],
         language: {
             emptyTable: "Nenhum dado disponível na tabela",
@@ -61,13 +62,18 @@ function Tabela_Planos(listaPlanos) {
 }
 
 $(document).ready(() => {
-
     $('#NomeRotina').text('Metas');
     $('#loadingModal').modal('hide')
     const hoje = new Date().toISOString().split('T')[0];
     document.getElementById('data-inicial').value = hoje;
     document.getElementById('data-final').value = hoje;
 
+    $('#cadastrar-faccionista').on('hidden.bs.modal', function () {
+        $('#form-cadastrar-faccionista').trigger('reset');
+        $('#form-cadastrar-faccionista').find('select').each(function () {
+            $(this).val($(this).find('option:first').val()).trigger('change');
+        });
+    });
 
 });
 
@@ -82,27 +88,28 @@ const Consulta_Faccionistas = async () => {
                 acao: 'Consulta_Faccionistas',
             }
         });
+
         const $select = $('#select-faccionista');
         $select.empty();
         $select.append('<option value="">Selecione o Faccionista (csw):</option>');
 
         data.forEach(item => {
-            $select.append(`<option value="${item.codFaccionista}">${item.codFaccionista} - ${item.nomeFaccionista}</option>`);
+            $select.append(`<option value="${item.codFaccionista} - ${item.nomeFaccionista}" data-cod="${item.codFaccionista}">${item.codFaccionista} - ${item.nomeFaccionista}</option>`);
         });
-        // Ativar o Select2 para o elemento
-        $('#select-faccionista').select2({
+
+        // Inicializar o Select2
+        $select.select2({
             theme: 'bootstrap4',
             width: '100%',
-            height: '30px',
             dropdownParent: $('#cadastrar-faccionista')
         });
-        $('.select2-selection__rendered').addClass('form-control')
     } catch (error) {
-        console.error('Erro ao consultar chamados:', error);
+        console.error('Erro ao consultar faccionistas:', error);
     } finally {
         $('#loadingModal').modal('hide');
     }
 };
+
 
 
 async function ConsultarMetas(congelado) {
@@ -168,11 +175,11 @@ async function ConsultarRealizadoGeral() {
 
 
 
-async function CadastrarFaccionista(congelado) {
+async function CadastrarFaccionista() {
     $('#loadingModal').modal('show');
     try {
         const dados = {
-            codFaccionista: $('#select-faccionista').val(),
+            codFaccionista: $('#select-faccionista').find('option:selected').data('cod'),
             apelido: $('#apelido-faccionista').val(),
             ArrayCategorias: [$('#select-categoria').val()],
             ArrayCapacidade: [$('#capacidade-dia').val()],
@@ -190,12 +197,19 @@ async function CadastrarFaccionista(congelado) {
             data: JSON.stringify(requestData),
         });
         console.log(response);
-        $('#cadastrar-faccionista').modal('hide')
-        Mensagem('Cadastro Realizado', 'success');
+        if (response[0]['Status'] === true) {
+            Mensagem(MensagemModal, 'success');
+            ConsultarMetasCategorias(true)
+        } else {
+            Mensagem('Erro', 'error')
+        }
+
+
 
     } catch (error) {
         console.error('Erro na solicitação AJAX:', error);
     } finally {
+        $('#cadastrar-faccionista').modal('hide')
         $('#loadingModal').modal('hide');
     }
 }
@@ -232,7 +246,6 @@ async function ConsultarMetasCategorias(congelado) {
 }
 
 function CriarTabelaMetas(listaMetas) {
-
     if ($.fn.DataTable.isDataTable('#table-metas')) {
         $('#table-metas').DataTable().destroy();
     }
@@ -247,11 +260,12 @@ function CriarTabelaMetas(listaMetas) {
         pageLength: 10,
         fixedHeader: true,
         dom: 'Bfrtip',
-        buttons: [{
+        buttons: [
+            {
                 text: '<i class="fa-solid fa-filter"></i>',
                 title: 'Filtros',
                 className: 'ButtonModal',
-                action: function(e, dt, node, config) {
+                action: function (e, dt, node, config) {
                     $('#filtrosModal').modal('show');
                 }
             },
@@ -259,62 +273,58 @@ function CriarTabelaMetas(listaMetas) {
                 text: '<i class="fa-solid fa-user-plus"></i>',
                 title: 'Adicionar Faccionista',
                 className: 'ButtonModal',
-                action: async function(e, dt, node, config) {
-                    await Consulta_Faccionistas();
-                    ConsultaCategoria()
+                action: async function (e, dt, node, config) {
                     $('#cadastrar-faccionista').modal('show');
+                    $('#btn-cadastrar-faccionista').text('Cadastrar');
+                    MensagemModal = 'Meta Cadastrada'
                 },
-
-            },
+            }
         ],
-        columns: [{
-                data: '01-nomeFac'
-            },
-            {
-                data: '03- categoria'
-            },
-            {
-                data: '04- AcordadoDia',
-                render: function(data) {
-                    return parseInt(data).toLocaleString();
-                }
-            },
+        columns: [
+            { data: '03- categoria' },
             {
                 data: '06-FaltaProgramar',
-                render: function(data) {
-                    return parseInt(data).toLocaleString();
-                }
-            },
-            {
-                data: '07-Fila',
-                render: function(data) {
-                    return parseInt(data).toLocaleString();
-                }
-            },
-            {
-                data: '08-Carga',
-                render: function(data) {
+                render: function (data) {
                     return parseInt(data).toLocaleString();
                 }
             },
             {
                 data: '09-Falta Produzir',
-                render: function(data) {
+                render: function (data) {
                     return parseInt(data).toLocaleString();
                 }
             },
             {
-                data: '10-dias'
+                data: '07-Fila',
+                render: function (data) {
+                    return parseInt(data).toLocaleString();
+                }
+            },
+            { data: '00- codFac', visible: false },
+            { data: '01-nomeFac' },
+            { data: 'nomefaccionistaCsw', visible: false },
+            {
+                data: '04- AcordadoDia',
+                render: function (data) {
+                    return parseInt(data).toLocaleString();
+                }
             },
             {
+                data: '08-Carga',
+                render: function (data) {
+                    return parseInt(data).toLocaleString();
+                }
+            },
+            { data: '10-dias' },
+            {
                 data: '11-Meta Dia',
-                render: function(data) {
+                render: function (data) {
                     return parseInt(data).toLocaleString();
                 }
             },
             {
                 data: 'Realizado',
-                render: function(data, type, row) {
+                render: function (data, type, row) {
                     const realizado = parseInt(data);
                     const meta = parseInt(row['11-Meta Dia']);
                     let icon = '';
@@ -327,22 +337,63 @@ function CriarTabelaMetas(listaMetas) {
 
                     return realizado.toLocaleString() + icon;
                 }
+            },
+            {
+                data: null, // Coluna para a porcentagem
+                render: function(data, type, row) {
+                    const realizado = parseInt(row['Realizado']);
+                    const meta = parseInt(row['11-Meta Dia']);
+                    if (meta === 0) {
+                        return '0%';
+                    }
+                    const percent = (realizado / meta * 100).toFixed(2);
+                    return `${percent}%`;
+                }
+            },
+            {
+                data: null,
+                render: function (row) {
+                    return `
+                    <div class="acoes">
+                        <button style="border: 1px solid rgb(235, 190, 6); border-radius: 7px; padding-left: 7px; padding-right: 7px; background-color: rgb(235, 190, 6);">
+                            <i class="fa-solid fa-pen-to-square" title="Editar" id="btnEditar"
+                                onclick="
+                                    $('#select-faccionista').val('${row['00- codFac']} - ${row['nomefaccionistaCsw']}').trigger('change');
+                                    $('#apelido-faccionista').val('${row['01-nomeFac']}');
+                                    $('#select-categoria').val('${row['03- categoria']}').trigger('change');
+                                    $('#capacidade-dia').val('${row['04- AcordadoDia']}');
+                                    $('#cadastrar-faccionista').modal('show');
+                                    $('#btn-cadastrar-faccionista').text('Editar');
+                                    MensagemModal = 'Meta Alterada';
+                                "
+                            ></i>
+                        </button> 
+                    </div>
+                `;
+                }
             }
         ],
+        rowCallback: function (row, data, index) {
+            if (data['01-nomeFac'] && data['01-nomeFac'].includes('EXCEDENTE')) {
+                $(row).css('background-color', '#FA8072');
+            }
+        },
         language: {
             paginate: {
                 first: 'Primeira',
                 previous: '<',
                 next: '>',
-                last: 'Última',
-            },
-        },
+                last: 'Última'
+            }
+        }
     });
 
-    window.filtrarTabelaMetas = function(categoria) {
-        tabela.column(1).search(categoria, true, false).draw();
+    window.filtrarTabelaMetas = function (categoria) {
+        tabela.column(0).search(categoria, true, false).draw();
     };
 }
+
+
 
 function CriarTabelaRealizado(listaMetas) {
 
@@ -360,14 +411,14 @@ function CriarTabelaRealizado(listaMetas) {
         pageLength: 10,
         fixedHeader: true,
         columns: [{
-                data: 'Realizado'
-            },
-            {
-                data: null, // Use null quando você não tem um único campo de dados para esta coluna
-                render: function(data, type, row) {
-                    return row.data + ' - ' + row.dia;
-                }
+            data: 'Realizado'
+        },
+        {
+            data: null, // Use null quando você não tem um único campo de dados para esta coluna
+            render: function (data, type, row) {
+                return row.data + ' - ' + row.dia;
             }
+        }
         ],
     });
 }
@@ -388,58 +439,58 @@ function CriarTabelaMetasCategorias(listaMetas) {
         pageLength: 10,
         fixedHeader: true,
         columns: [{
-                data: 'categoria'
-            },
-            {
-                data: 'FaltaProgramar',
-                render: function(data) {
-                    return parseInt(data).toLocaleString();
-                }
-            },
-            {
-                data: 'Fila',
-                render: function(data) {
-                    return parseInt(data).toLocaleString();
-                }
-            },
-            {
-                data: 'Carga Atual',
-                render: function(data) {
-                    return parseInt(data).toLocaleString();
-                }
-            },
-            {
-                data: 'Falta Produzir',
-                render: function(data) {
-                    return parseInt(data).toLocaleString();
-                }
-            },
-            {
-                data: 'dias'
-
-            },
-            {
-                data: 'Meta Dia',
-                render: function(data) {
-                    return parseInt(data).toLocaleString();
-                }
-            },
-            {
-                data: 'Realizado',
-                render: function(data, type, row) {
-                    const realizado = parseInt(data);
-                    const meta = parseInt(row['Meta Dia']);
-                    let icon = '';
-
-                    if (realizado < meta) {
-                        icon = ' <i class="fas fa-arrow-down" style="color: red; float: right;"></i>';
-                    } else if (realizado > meta) {
-                        icon = ' <i class="fas fa-arrow-up" style="color: green; float: right;"></i>';
-                    }
-
-                    return realizado.toLocaleString() + icon;
-                }
+            data: 'categoria'
+        },
+        {
+            data: 'FaltaProgramar',
+            render: function (data) {
+                return parseInt(data).toLocaleString();
             }
+        },
+        {
+            data: 'Fila',
+            render: function (data) {
+                return parseInt(data).toLocaleString();
+            }
+        },
+        {
+            data: 'Carga Atual',
+            render: function (data) {
+                return parseInt(data).toLocaleString();
+            }
+        },
+        {
+            data: 'Falta Produzir',
+            render: function (data) {
+                return parseInt(data).toLocaleString();
+            }
+        },
+        {
+            data: 'dias'
+
+        },
+        {
+            data: 'Meta Dia',
+            render: function (data) {
+                return parseInt(data).toLocaleString();
+            }
+        },
+        {
+            data: 'Realizado',
+            render: function (data, type, row) {
+                const realizado = parseInt(data);
+                const meta = parseInt(row['Meta Dia']);
+                let icon = '';
+
+                if (realizado < meta) {
+                    icon = ' <i class="fas fa-arrow-down" style="color: red; float: right;"></i>';
+                } else if (realizado > meta) {
+                    icon = ' <i class="fas fa-arrow-up" style="color: green; float: right;"></i>';
+                }
+
+                return realizado.toLocaleString() + icon;
+            }
+        }
         ],
         language: {
             paginate: {
@@ -452,7 +503,7 @@ function CriarTabelaMetasCategorias(listaMetas) {
     });
     let ultimaCategoriaClicada = null;
 
-    $('#table-categorias tbody').on('click', 'tr', function() {
+    $('#table-categorias tbody').on('click', 'tr', function () {
         const categoria = $(this).find('td').first().text().trim();
 
         // Adiciona um console.log para verificar o clique
@@ -553,6 +604,8 @@ async function SelecaoLote(congelado) {
 
     await ConsultarMetasCategorias(congelado)
     await ConsultarMetas(congelado);
+    await Consulta_Faccionistas();
+    await ConsultaCategoria()
     ConsultarRealizadoGeral()
     $('#table-metas').removeClass('d-none');
     $('#table-categorias').removeClass('d-none');
