5d68d2b759f36ed598dddb37788f34ed964186b8
Luis Fernando Gonçalves <80552965+luis-fe@users.noreply.github.com>
2025-01-06 16:51:22 -0300

    Update index.php

--- nova_versao/Pcp/PlanoDeProducao/index.php
+++ nova_versao/Pcp/PlanoDeProducao/index.php
@@ -1,7 +1,7 @@
 <?php
 include_once('requests.php');
 include_once("../../templates/Loading.php");
-include_once('../../templates/header.php');
+include_once('../../templates/headerPcp.php');
 ?>
 <link rel="stylesheet" href="style.css">
 <style>
@@ -526,7 +526,7 @@ include_once('../../templates/header.php');
 </div>
 
 <?php
-include_once('../../templates/footer.php');
+include_once('../../templates/footerPcp.php');
 ?>
 
 <script src="script.js"></script>
