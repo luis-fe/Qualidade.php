faf3d81d367b8fbe934af685886c75333ba5abd6
Luis Fernando Gonçalves <80552965+luis-fe@users.noreply.github.com>
2025-01-09 13:23:01 -0300

    Add files via upload

---
+++ nova_versao/Pcp/AnaliseNecessidade/index.php
@@ -0,0 +1,80 @@
+<?php
+include_once('requests.php');
+include_once("../../templates/Loading.php");
+include_once('../../templates/header.php');
+?>
+<!-- Adicione o CSS do Select2 -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.13/css/select2.min.css">
+<link rel="stylesheet" href="style.css">
+
+<div class="titulo-tela">
+    <span class="span-icone"><i class="bi bi-bag-check"></i></span> Análise de Materiais
+</div>
+
+<div class="mt-3 row justify-content-center" id="selecao-plano">
+    <form id="form-vendas" class="row" onsubmit="Analise_Materiais(); return false;">
+        <div class="col-12 col-md-6">
+            <div class="select text-start">
+                <label for="select-plano" class="form-label">Selecionar plano</label>
+                <select id="select-plano" class="form-select" required>
+                </select>
+            </div>
+        </div>
+
+        <div class="col-12 col-md-4">
+            <div class="select text-start">
+                <label for="select-pedidos-bloqueados" class="form-label">Considera pedidos bloqueados?</label>
+                <select id="select-pedidos-bloqueados" class="form-select" required>
+                    <option></option>
+                    <option value="sim">Sim</option>
+                    <option value="nao">Não</option>
+                </select>
+            </div>
+        </div>
+
+        <div class="col-12 col-md-2 d-flex align-items-end justify-content-center">
+            <button type="submit" class="btn btn-geral w-100" style="margin-top: 32px;">Consultar</button>
+        </div>
+    </form>
+</div>
+
+<div class="col-12 div-analise" style="background-color: lightgray; border-radius: 8px;">
+    <div class="div-tabela" style="max-width: 100%; overflow: auto;">
+        <table class="table table-bordered table-striped" id="table-analise" style="width: 100%;">
+            <thead>
+                <tr>
+                    <th>Código<br><input type="search" class="search-input form-control"></th>
+                    <th>Descrição<br><input type="search" class="search-input form-control"></th>
+                    <th>Código red.<br><input type="search" class="search-input form-control"></th>
+                    <th>Necessidade<br><input type="search" class="search-input form-control"></th>
+                    <th>Estoque Atual<br><input type="search" class="search-input form-control"></th>
+                    <th>Compra<br><input type="search" class="search-input form-control"></th>
+                    <th>Requisições<br><input type="search" class="search-input form-control"></th>
+                    <th>Fornecedor Principal<br><input type="search" class="search-input form-control"></th>
+                    <th>Medida<br><input type="search" class="search-input form-control"></th>
+                    <th>Lote Mínimo<br><input type="search" class="search-input form-control"></th>
+                    <th>Lote Múltiplo<br><input type="search" class="search-input form-control"></th>
+                    <th>Lead Time<br><input type="search" class="search-input form-control"></th>
+                    <th>Fator de Conversão<br><input type="search" class="search-input form-control"></th>
+                </tr>
+            </thead>
+            <tbody>
+                <!-- Aqui vão os dados da tabela -->
+            </tbody>
+        </table>
+    </div>
+    <div class="custom-pagination-container pagination-analise d-md-flex col-12 text-center text-md-start">
+        <div id="custom-info" class="col-12 col-md-6 mb-2 mb-md-0">
+            <label for="text">Itens por página</label>
+            <input id="itens-analise" class="input-itens" type="text" value="10" min="1">
+        </div>
+        <div id="pagination-analise" class="col-12 col-md-6 d-flex justify-content-center justify-content-md-end">
+        </div>
+    </div>
+</div>
+
+
+<?php
+include_once('../../templates/footer.php');
+?>
+<script src="script1.js"></script>
---
+++ nova_versao/Pcp/AnaliseNecessidade/script1.js
@@ -0,0 +1,161 @@
+$(document).ready(async () => {
+    Consulta_Planos();
+    $('#select-plano').select2({
+        placeholder: "Selecione um plano",
+        allowClear: false,
+        width: '100%'
+    });
+
+    $('#select-pedidos-bloqueados').select2({
+        placeholder: "Pedidos Bloqueados?",
+        allowClear: false,
+        width: '100%'
+    });
+
+    $('#btn-vendas').addClass('btn-menu-clicado')
+});
+
+function Consulta_Planos() {
+    $('#loadingModal').modal('show');
+    $.ajax({
+        type: 'GET',
+        url: 'requests.php',
+        dataType: 'json',
+        data: {
+            acao: 'Consulta_Planos',
+        },
+        success: function (data) {
+            $('#select-plano').empty();
+            $('#select-plano').append('<option value="" disabled selected>Selecione um plano...</option>');
+            data.forEach(function (plano) {
+                $('#select-plano').append(`
+                        <option value="${plano['01- Codigo Plano']}">
+                            ${plano['01- Codigo Plano']} - ${plano['02- Descricao do Plano']}
+                        </option>
+                    `);
+            });
+            $('#loadingModal').modal('hide');
+        },
+        error: function (xhr, status, error) {
+            console.error('Erro ao consultar planos:', error);
+            $('#loadingModal').modal('hide');
+        }
+    });
+}
+
+async function Analise_Materiais() {
+    $('#loadingModal').modal('show');
+    try {
+        const requestData = {
+            acao: "Analise_Materiais",
+            dados: {
+                "codPlano": $('#select-plano').val(),
+                "consideraPedidosBloqueado": $('#select-pedidos-bloqueados').val()
+            }
+        };
+
+        const response = await $.ajax({
+            type: 'POST',
+            url: 'requests.php',
+            contentType: 'application/json',
+            data: JSON.stringify(requestData),
+        });
+        TabelaAnalise(response);
+    } catch (error) {
+        console.error('Erro na solicitação AJAX:', error);
+        Mensagem_Canto('Erro', 'error')
+    } finally {
+        $('#loadingModal').modal('hide');
+    }
+}
+
+
+function TabelaAnalise(listaAnalise) {
+    if ($.fn.DataTable.isDataTable('#table-analise')) {
+        $('#table-analise').DataTable().destroy();
+    }
+
+    const tabela = $('#table-analise').DataTable({
+        searching: true,
+        paging: true,
+        lengthChange: false,
+        info: false,
+        pageLength: 10,
+        data: listaAnalise,
+        columns: [{
+            data: '02-codCompleto'
+        },
+        {
+            data: '03-descricaoComponente'
+        },
+        {
+            data: '01-codReduzido'
+        },
+        {
+            data: '10-Necessidade Compra (Tendencia)'
+        },
+        {
+            data: '08-estoqueAtual'
+        },
+        {
+            data: '09-SaldoPedCompras'
+        },
+        {
+            data: '07-EmRequisicao'
+        },
+        {
+            data: '04-fornencedorPreferencial'
+        },
+        {
+            data: '05-unidade'
+        },
+        {
+            data: '14-Lote Mínimo'
+        },
+        {
+            data: '11-Lote Mutiplo'
+        },
+        {
+            data: '13-LeadTime'
+        },
+        {
+            data: 'fatorConversao'
+        },
+        ],
+        language: {
+            paginate: {
+                previous: '<i class="fa-solid fa-backward-step"></i>',
+                next: '<i class="fa-solid fa-forward-step"></i>'
+            },
+            info: "Página _PAGE_ de _PAGES_",
+            emptyTable: "Nenhum dado disponível na tabela",
+            zeroRecords: "Nenhum registro encontrado"
+        },
+        drawCallback: function () {
+            $('#pagination-analise').html($('.dataTables_paginate').html());
+            $('#pagination-analise span').remove();
+            $('#pagination-analise a').off('click').on('click', function (e) {
+                e.preventDefault();
+                if ($(this).hasClass('previous')) tabela.page('previous').draw('page');
+                if ($(this).hasClass('next')) tabela.page('next').draw('page');
+            });
+            $('.dataTables_paginate').hide();
+        }
+    });
+
+    $('.search-input-analise').on('input', function () {
+        tabela.column($(this).closest('th').index()).search($(this).val()).draw();
+    });
+
+    $('#btn-selecionar-colecoes').off('click').on('click', () => {
+        ColecoesSelecionadas = tabela.rows().nodes().toArray()
+            .filter(row => $(row).find('.row-checkbox').is(':checked'))
+            .map(row => String(tabela.row(row).data().codColecao));
+        if (ColecoesSelecionadas.length === 0) {
+            Mensagem('Nenhuma Coleção selecionada!', 'warning');
+        } else {
+            Vincular_Colecoes();
+
+        }
+    });
+}
