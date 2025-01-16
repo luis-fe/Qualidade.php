56932fc06651cd73460df928fbce749a5b5c8704
luis-fe <luisengp@gmail.com>
2025-01-16 11:23:19 -0300

    usuarioSenha 2

--- ConexaoCSW.py
+++ ConexaoCSW.py
@@ -43,7 +43,7 @@ def ConexaoInternoMPL():
         senha = os.getenv('CSW_PASSWORD')
         user = os.getenv('CSW_USER')
 
-        print(f'usuario {user}')
+        print(f'usuario {user}-{senha}')
 
         conn = jaydebeapi.connect(
             'com.intersys.jdbc.CacheDriver',
