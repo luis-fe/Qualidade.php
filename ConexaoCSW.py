be314c831edbdbf581af526ea6e7af827c83bf21
luis-fe <luisengp@gmail.com>
2025-01-16 11:29:45 -0300

    usuarioSenha 2

--- ConexaoCSW.py
+++ ConexaoCSW.py
@@ -47,8 +47,8 @@ def ConexaoInternoMPL():
 
         conn = jaydebeapi.connect(
             'com.intersys.jdbc.CacheDriver',
-            'jdbc:Cache://192.168.0.25:1972/CONSISTEM',
-            {'user': 'root', 'password': 'ccscache'},
+            f'jdbc:Cache://{host}/CONSISTEM',
+            {'user': f'{user}', 'password': f'{senha}'},
             'CacheDB_root.jar'
         )
         return conn
