ff56086fc91d7be1494c58607fab14e2bc6c9509
Luis Fernando Gonçalves <80552965+luis-fe@users.noreply.github.com>
2025-01-24 11:19:39 -0300

    Update requests.php

--- nova_versao/Pcp/TendenciaSku/requests.php
+++ nova_versao/Pcp/TendenciaSku/requests.php
@@ -4,7 +4,7 @@ if (isset($_SESSION['username']) && isset($_SESSION['empresa'])) {
     $username = $_SESSION['username'];
     $empresa = $_SESSION['empresa'];
 } else {
-    header("Location: ../../indexPcp.php");
+    header("Location: ../../login.php");
 }
 
 
@@ -28,6 +28,20 @@ switch ($_SERVER["REQUEST_METHOD"]) {
                     $plano = $_GET['plano'];
                     jsonResponse(ConsultaAbcPlano('1', $plano));
                     break;
+                case 'Consulta_Simulacoes':
+                    jsonResponse(ConsultaSimulacoes('1'));
+                    break;
+                case 'Consulta_Categorias':
+                    jsonResponse(ConsultaCategorias('1'));
+                    break;
+                case 'Consulta_Abc':
+                    jsonResponse(ConsultaAbc('1'));
+                    break;
+                case 'Consulta_Simulacao_Especifica':
+                    $simulacao = urldecode($_GET['simulacao']);
+                    jsonResponse(ConsultaSimulacaoEspecifica('1', $simulacao));
+                    break;
+
                 default:
                     jsonResponse(['status' => false, 'message' => 'Ação GET não reconhecida.']);
                     break;
@@ -50,7 +64,11 @@ switch ($_SERVER["REQUEST_METHOD"]) {
                     header('Content-Type: application/json');
                     echo json_encode(Simular_Programacao('1', $dadosObjeto));
                     break;
-
+                case 'Cadastro_Simulacao':
+                    $dadosObjeto = (object) $dados;
+                    header('Content-Type: application/json');
+                    echo json_encode(CadastroSimulacao('1', $dadosObjeto));
+                    break;
                 default:
                     jsonResponse(['status' => false, 'message' => 'Ação POST não reconhecida.']);
                     break;
@@ -98,6 +116,95 @@ function ConsultarPlanos($empresa)
     return json_decode($apiResponse, true);
 }
 
+function ConsultaSimulacaoEspecifica($empresa, $simulacao)
+{
+    $baseUrl = ($empresa == "1") ? 'http://192.168.0.183:8000' : 'http://192.168.0.184:8000';
+    $apiUrl = "{$baseUrl}/pcp/api/consultaDetalhadaSimulacao?nomeSimulacao=" . urlencode($simulacao);
+    $ch = curl_init($apiUrl);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, [
+        'Content-Type: application/json',
+        "Authorization: a44pcp22",
+    ]);
+
+    $apiResponse = curl_exec($ch);
+
+    if (!$apiResponse) {
+        error_log("Erro na requisição: " . curl_error($ch), 0);
+    }
+
+    curl_close($ch);
+
+    return json_decode($apiResponse, true);
+}
+
+
+function ConsultaCategorias($empresa)
+{
+    $baseUrl = ($empresa == "1") ? 'http://192.168.0.183:8000' : 'http://192.168.0.184:8000';
+    $apiUrl = "{$baseUrl}/pcp/api/CategoriasDisponiveis";
+    $ch = curl_init($apiUrl);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, [
+        'Content-Type: application/json',
+        "Authorization: a44pcp22",
+    ]);
+
+    $apiResponse = curl_exec($ch);
+
+    if (!$apiResponse) {
+        error_log("Erro na requisição: " . curl_error($ch), 0);
+    }
+
+    curl_close($ch);
+
+    return json_decode($apiResponse, true);
+}
+
+function ConsultaAbc($empresa)
+{
+    $baseUrl = ($empresa == "1") ? 'http://192.168.0.183:8000' : 'http://192.168.0.184:8000';
+    $apiUrl = "{$baseUrl}/pcp/api/consultaParametrizacaoABC";
+    $ch = curl_init($apiUrl);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, [
+        'Content-Type: application/json',
+        "Authorization: a44pcp22",
+    ]);
+
+    $apiResponse = curl_exec($ch);
+
+    if (!$apiResponse) {
+        error_log("Erro na requisição: " . curl_error($ch), 0);
+    }
+
+    curl_close($ch);
+
+    return json_decode($apiResponse, true);
+}
+
+function ConsultaSimulacoes($empresa)
+{
+    $baseUrl = ($empresa == "1") ? 'http://192.168.0.183:8000' : 'http://192.168.0.184:8000';
+    $apiUrl = "{$baseUrl}/pcp/api/ConsultaSimulacoes";
+    $ch = curl_init($apiUrl);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, [
+        'Content-Type: application/json',
+        "Authorization: a44pcp22",
+    ]);
+
+    $apiResponse = curl_exec($ch);
+
+    if (!$apiResponse) {
+        error_log("Erro na requisição: " . curl_error($ch), 0);
+    }
+
+    curl_close($ch);
+
+    return json_decode($apiResponse, true);
+}
+
 function ConsultaAbcPlano($empresa, $plano)
 {
     $baseUrl = ($empresa == "1") ? 'http://192.168.0.183:8000' : 'http://192.168.0.184:8000';
@@ -119,6 +226,7 @@ function ConsultaAbcPlano($empresa, $plano)
 
     return json_decode($apiResponse, true);
 }
+
 
 function ConsultaTendencias($empresa, $dados)
 {
@@ -156,6 +264,39 @@ function Simular_Programacao($empresa, $dados)
 {
     $baseUrl = ($empresa == "1") ? 'http://192.168.0.183:8000' : 'http://192.168.0.184:8000';
     $apiUrl = "{$baseUrl}/pcp/api/simulacaoProgramacao";
+
+    $ch = curl_init($apiUrl);
+
+    $options = [
+        CURLOPT_CUSTOMREQUEST => "POST",
+        CURLOPT_POSTFIELDS => json_encode($dados),
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_HTTPHEADER => [
+            'Content-Type: application/json',
+            "Authorization: a44pcp22",
+        ],
+    ];
+
+    curl_setopt_array($ch, $options);
+
+    $apiResponse = curl_exec($ch);
+
+    if (curl_errno($ch)) {
+        $error = curl_error($ch);
+        error_log("Erro na solicitação cURL: {$error}");
+        return false;
+    }
+
+    curl_close($ch);
+
+    return json_decode($apiResponse, true);
+}
+
+
+function CadastroSimulacao($empresa, $dados)
+{
+    $baseUrl = ($empresa == "1") ? 'http://192.168.0.183:8000' : 'http://192.168.0.184:8000';
+    $apiUrl = "{$baseUrl}/pcp/api/atualizaInserirSimulacao";
 
     $ch = curl_init($apiUrl);
 
