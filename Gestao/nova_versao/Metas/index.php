f97f424be77f3e82d00f7d6c0b1d76a3c2294d16
Luis Fernando Gonçalves <80552965+luis-fe@users.noreply.github.com>
2025-03-18 20:36:05 -0300

    Update index.php

--- Gestao/nova_versao/Metas/index.php
+++ Gestao/nova_versao/Metas/index.php
@@ -1,7 +1,7 @@
 <?php
 include_once('requests.php');
-include_once("../../templates/Loading.php");
-include_once('../../templates/header.php');
+include_once("../../../templates/LoadingGestao.php");
+include_once('../../../templates/headerGestao.php');
 ?>
 <!-- Adicione o CSS do Select2 -->
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.13/css/select2.min.css">
@@ -237,6 +237,6 @@ include_once('../../templates/header.php');
 </div>
 
 <?php
-include_once('../../templates/footer.php');
+include_once('../../../templates/footerGestao.php');
 ?>
 <script src="script.js"></script>
