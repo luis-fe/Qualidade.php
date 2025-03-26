42148d4bb8e7fb903ba586d6651d752d315ea2dc
Luis Fernando Gonçalves <80552965+luis-fe@users.noreply.github.com>
2025-03-26 08:57:44 -0300

    Update requests.php

--- nova_versao/Pcp/ABC/requests.php
+++ nova_versao/Pcp/ABC/requests.php
@@ -21,9 +21,38 @@ switch ($_SERVER["REQUEST_METHOD"]) {
         if (isset($_GET["acao"])) {
             $acao = $_GET["acao"];
             switch ($acao) {
-                case 'Consulta_Abc':
-                    jsonResponse(ConsultarAbc('1'));
+                case 'Consulta_Planos':
+                    jsonResponse(ConsultarPlanos('1'));
                     break;
+                case 'Consulta_Lotes':
+                    $plano = $_GET['plano'];
+                    jsonResponse(ConsultarLotes('1', $plano));
+                    break;
+                case 'Consultar_Realizados':
+                    $Fase = $_GET['Fase'];
+                    $dataInicial = $_GET['dataInicial'];
+                    $dataFinal = $_GET['dataFinal'];
+                    jsonResponse(ConsultarRealizados('1', $Fase, $dataInicial, $dataFinal));
+                    break;
+                case 'Consultar_Cronograma':
+                    $plano = $_GET['plano'];
+                    $fase = $_GET['fase'];
+                    jsonResponse(ConsultarCronograma('1', $plano, $fase));
+                    break;
+                case 'Consultar_Tipo_Op':
+                    jsonResponse(ConsultarTipoOp('1'));
+                    break;
+                case 'Consulta_Previsao_Categoria':
+                    $fase = $_GET['fase'];
+                    jsonResponse(ConsultaPrevisaoCategoria($fase));
+                    break;
+                case 'Consulta_Falta_Produzir_Categoria':
+                    $fase = $_GET['fase'];
+                    $plano = $_GET['plano'];
+                    jsonResponse(ConsultaFaltaProduzirCategoria_Fase($fase, $plano));
+                    break;
+
+
                 default:
                     jsonResponse(['status' => false, 'message' => 'Ação GET não reconhecida.']);
                     break;
@@ -36,9 +65,9 @@ switch ($_SERVER["REQUEST_METHOD"]) {
         $dados = $requestData['dados'] ?? null;
         if ($acao) {
             switch ($acao) {
-                case 'Cadastrar_Parametro':
+                case 'Consulta_Metas':
                     header('Content-Type: application/json');
-                    echo (CadastrarParametro('1', $dados));
+                    echo json_encode(ConsultarMetas('1', $dados));
                     break;
                 default:
                     jsonResponse(['status' => false, 'message' => 'Ação POST não reconhecida.']);
@@ -63,10 +92,11 @@ switch ($_SERVER["REQUEST_METHOD"]) {
         break;
 }
 
-function ConsultarAbc($empresa)
+
+function ConsultarLotes($empresa, $plano)
 {
-    $baseUrl = ($empresa == "1") ? 'http://192.168.0.183:8000' : 'http://10.162.0.191:8000';
-    $apiUrl = "{$baseUrl}/pcp/api/consultaParametrizacaoABC";
+    $baseUrl = ($empresa == "1") ? 'http://192.168.0.183:8000' : 'http://192.168.0.183:8000';
+    $apiUrl = "{$baseUrl}/pcp/api/ConsultaLotesVinculados?plano={$plano}";
     $ch = curl_init($apiUrl);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     curl_setopt($ch, CURLOPT_HTTPHEADER, [
@@ -85,10 +115,146 @@ function ConsultarAbc($empresa)
     return json_decode($apiResponse, true);
 }
 
-function CadastrarParametro($empresa, $dados)
+function ConsultaPrevisaoCategoria($Fase)
 {
-    $baseUrl = ($empresa == "1") ? 'http://192.168.0.183:8000' : 'http://10.162.0.191:8000';
-    $apiUrl = "{$baseUrl}/pcp/api/CadastrarParametroABC";
+    $fase_encoded = urlencode($Fase);
+    $baseUrl = 'http://192.168.0.183:8000/pcp';
+    $apiUrl = "{$baseUrl}/api/previsaoCategoriaFase?nomeFase={$fase_encoded}";
+    $ch = curl_init($apiUrl);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, [
+        'Content-Type: application/json',
+        "Authorization: a44pcp22",
+    ]);
+
+    $apiResponse = curl_exec($ch);
+
+    if (!$apiResponse) {
+        error_log("Erro na requisição: " . curl_error($ch), 0);
+    }
+
+    curl_close($ch);
+
+    return json_decode($apiResponse, true);
+}
+
+function ConsultaFaltaProduzirCategoria_Fase($Fase, $Plano)
+{
+    $fase_encoded = urlencode($Fase);
+    $baseUrl = 'http://192.168.0.183:8000/pcp';
+    $apiUrl = "{$baseUrl}/api/FaltaProduzircategoria_fase?nomeFase={$fase_encoded}&codPlano={$Plano}";
+    $ch = curl_init($apiUrl);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, [
+        'Content-Type: application/json',
+        "Authorization: a44pcp22",
+    ]);
+
+    $apiResponse = curl_exec($ch);
+
+    if (!$apiResponse) {
+        error_log("Erro na requisição: " . curl_error($ch), 0);
+    }
+
+    curl_close($ch);
+
+    return json_decode($apiResponse, true);
+}
+
+
+function ConsultarTipoOp($empresa)
+{
+    $baseUrl = ($empresa == "1") ? 'http://192.168.0.183:8000' : 'http://192.168.0.183:8000';
+    $apiUrl = "{$baseUrl}/pcp/api/filtroProdutivo";
+    $ch = curl_init($apiUrl);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, [
+        'Content-Type: application/json',
+        "Authorization: a44pcp22",
+    ]);
+
+    $apiResponse = curl_exec($ch);
+
+    if (!$apiResponse) {
+        error_log("Erro na requisição: " . curl_error($ch), 0);
+    }
+
+    curl_close($ch);
+
+    return json_decode($apiResponse, true);
+}
+
+function ConsultarPlanos($empresa)
+{
+    $baseUrl = ($empresa == "1") ? 'http://192.168.0.183:8000' : 'http://192.168.0.183:8000';
+    $apiUrl = "{$baseUrl}/pcp/api/Plano";
+    $ch = curl_init($apiUrl);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, [
+        'Content-Type: application/json',
+        "Authorization: a44pcp22",
+    ]);
+
+    $apiResponse = curl_exec($ch);
+
+    if (!$apiResponse) {
+        error_log("Erro na requisição: " . curl_error($ch), 0);
+    }
+
+    curl_close($ch);
+
+    return json_decode($apiResponse, true);
+}
+
+function ConsultarRealizados($empresa, $Fase, $dataInicio, $dataFinal)
+{
+    $fase_encoded = urlencode($Fase);
+    $baseUrl = ($empresa == "1") ? 'http://192.168.0.183:8000' : 'http://192.168.0.183:8000';
+    $apiUrl = "{$baseUrl}/pcp/api/RetornoPorFaseDiaria?nomeFase={$fase_encoded}&dataInicio={$dataInicio}&dataFinal={$dataFinal}&codEmpresa={$empresa}";
+    $ch = curl_init($apiUrl);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, [
+        'Content-Type: application/json',
+        "Authorization: a44pcp22",
+    ]);
+
+    $apiResponse = curl_exec($ch);
+
+    if (!$apiResponse) {
+        error_log("Erro na requisição: " . curl_error($ch), 0);
+    }
+
+    curl_close($ch);
+
+    return json_decode($apiResponse, true);
+}
+
+function ConsultarCronograma($empresa, $codPlano, $codFase)
+{
+    $baseUrl = ($empresa == "1") ? 'http://192.168.0.183:8000' : 'http://192.168.0.183:8000';
+    $apiUrl = "{$baseUrl}/pcp/api/ConsultaCronogramaFasePlanoFase?codigoPlano={$codPlano}&codFase={$codFase}";
+    $ch = curl_init($apiUrl);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, [
+        'Content-Type: application/json',
+        "Authorization: a44pcp22",
+    ]);
+
+    $apiResponse = curl_exec($ch);
+
+    if (!$apiResponse) {
+        error_log("Erro na requisição: " . curl_error($ch), 0);
+    }
+
+    curl_close($ch);
+
+    return json_decode($apiResponse, true);
+}
+
+function ConsultarMetas($empresa, $dados)
+{
+    $baseUrl = ($empresa == "1") ? 'http://192.168.0.183:8000' : 'http://192.168.0.183:8000';
+    $apiUrl = "{$baseUrl}/pcp/api/MetasFases";
 
     $ch = curl_init($apiUrl);
 
@@ -108,18 +274,12 @@ function CadastrarParametro($empresa, $dados)
 
     if (curl_errno($ch)) {
         $error = curl_error($ch);
-        $response = [
-            'status' => false,
-            'message' => "Erro na solicitação cURL: {$error}"
-        ];
-    } else {
-        $response = [
-            'status' => true,
-            'resposta' => json_decode($apiResponse, true)
-        ];
+        error_log("Erro na solicitação cURL: {$error}");
+        return false;
     }
 
     curl_close($ch);
 
-    return json_encode($response);
+    return json_decode($apiResponse, true);
 }
+
