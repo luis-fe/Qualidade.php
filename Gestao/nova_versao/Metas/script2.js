98ef57714f639a00007d1b1bd0ef5b5d48e6c233
Luis Fernando Gonçalves <80552965+luis-fe@users.noreply.github.com>
2025-04-29 16:47:47 -0300

    nova mudanca

--- Gestao/nova_versao/Metas/script2.js
+++ Gestao/nova_versao/Metas/script2.js
@@ -141,7 +141,7 @@ const Consulta_Falta_Produzir_Categoria = async (Fase, Plano) => {
         TabelaFaltaProduzirCategorias(response);
         console.log(response)
         // Atualiza o título do modal com a fase
-        $('#modal-falta-produzir-categorias').text(`Falta Produzir - ${Fase}`);
+        $('#titulo-falta-produzir').text(`Falta Produzir - ${Fase}`);
         $('#modal-falta-produzir-categorias').modal('show');
 
     } catch (error) {
