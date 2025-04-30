a9be2833216f9e7b258c4823bf71e5efae6ab28f
Luis Fernando Gonçalves <80552965+luis-fe@users.noreply.github.com>
2025-04-30 15:48:40 -0300

    teste novo

--- Gestao/nova_versao/Metas/index.php
+++ Gestao/nova_versao/Metas/index.php
@@ -257,12 +257,12 @@ include_once('../../../templates/headerGestao.php');
                 <button type="button" class="btn-close-custom" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body" style="max-height: 1000px; overflow: auto">
-                <table class="table table-bordered table-striped" id="table-cargaOP_fase" style="width: 150%;">
+                <table class="table table-bordered table-striped" id="table-cargaOP_fase" style="width: 80%;">
                     <thead id = 'CabecalhoModal'>
                         <tr >
-                            <th>COLECAO<br><input type="search" class="search-input-table-cargaOP_fase" style="min-width: 130px;"></th>
+                            <th>COLECAO<br><input type="search" class="search-input-table-cargaOP_fase" style="min-width: 100px;"></th>
                             <th>numeroOP<br></th>
-                            <th>categoria<br><input type="search" class="search-input-table-cargaOP_fase" style="min-width: 130px;"></th>
+                            <th>categoria<br><input type="search" class="search-input-table-cargaOP_fase" style="min-width: 100px;"></th>
                             <th>codProduto<br></th>
                             <th id ="formato_descricao">descricao<br></th>
                             <th>Carga<br></th>
