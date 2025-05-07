9b1a58e6cc63090fab90a57632dbf21d1d35b230
Luis Fernando Gonçalves <80552965+luis-fe@users.noreply.github.com>
2025-05-07 17:49:48 -0300

    teste adeq

--- Gestao/nova_versao/Metas/index.php
+++ Gestao/nova_versao/Metas/index.php
@@ -248,13 +248,13 @@ include_once('../../../templates/headerGestao.php');
 
 
 <div class="modal fade modal-custom" id="modal-realizadoDia" tabindex="-1" aria-labelledby="customModalLabel" aria-hidden="true">
-    <div class="modal-dialog modal-dialog-top">
-        <div class="modal-content">
+    <div class="modal-dialog modal-dialog-centered w-100 m-0">
+        <div class="modal-content w-100">
             <div class="modal-header">
                 <h5 class="modal-title" id="titulo-realizadoDia" style="color: black;"></h5>
                 <button type="button" class="btn-close-custom" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
-            <div class="modal-body" style="max-height: 500px; overflow: auto">
+            <div class="modal-body" style="max-height: 800px; overflow: auto">
                 <table class="table table-bordered table-striped" id="table-realizadoDia" style="width: 100%;">
                     <thead id = 'CabecalhoModal'>
                         <tr>
