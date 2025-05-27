19f6483be90dc5ae64304deb587571289c16e79d
Luis Fernando Gonçalves <80552965+luis-fe@users.noreply.github.com>
2025-05-27 08:15:59 -0300

    incrementando api de produtividade das caixas

--- routes/automacaoWMS_CSW.py
+++ routes/automacaoWMS_CSW.py
@@ -130,6 +130,7 @@ def RecarregarPedidos():
     ReservaEnderecos.ReservaPedidosNaoRepostos(empresa,natureza,bool(consideraSobra),ordem,int(5),'Retirar Substitutos')
     ReservaEnderecos.ReservaPedidosNaoRepostos(empresa,'7',bool(consideraSobra),ordem,int(5),'Retirar Substitutos')
     ReservaEnderecos.ReservaPedidosNaoRepostos(empresa,natureza,bool(True),'desc',int(5),'Retirar Substitutos')
+    ReservaEnderecos.ReservaPedidosNaoRepostos(empresa,'7',bool(True),'desc',int(5),'Retirar Substitutos')
     ReservaEnderecos.ReservaPedidosNaoRepostos(empresa,natureza,bool(consideraSobra),ordem,int(3),'Substitutos')
     ReservaEnderecos.ReservaPedidosNaoRepostos(empresa,natureza,bool(True),'desc',int(3),'')
 
