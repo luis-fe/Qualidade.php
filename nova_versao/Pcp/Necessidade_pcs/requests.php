626218320502f78171908b36be46c1e645c6b013
Luis Fernando Gonçalves <80552965+luis-fe@users.noreply.github.com>
2025-06-03 14:28:10 -0300

    Update requests.php

--- nova_versao/Pcp/Necessidade_pcs/requests.php
+++ nova_versao/Pcp/Necessidade_pcs/requests.php
@@ -26,34 +26,27 @@ switch ($_SERVER["REQUEST_METHOD"]) {
                 case 'Consulta_Naturezas':
                     jsonResponse(ConsultarNaturezas('1'));
                     break;
-                case 'Consulta_Comprometidos':
-                    jsonResponse(ConsultarComprometidos('1'));
-                    break;
-                case 'Consulta_Comprometidos_Compras':
-                    jsonResponse(ConsultarComprometidoCompras('1'));
-                    break;
                 case 'Consulta_Simulacoes':
                     jsonResponse(ConsultaSimulacoes('1'));
-                    break;
-                case 'Consulta_Abc_Plano':
-                    $plano = $_GET['plano'];
-                    jsonResponse(ConsultaAbcPlano('1', $plano));
-                    break;
-                 case 'Consulta_Categorias':
-                    jsonResponse(ConsultaCategorias('1'));
                     break;
                 case 'Consulta_Simulacao_Especifica':
                     $simulacao = urldecode($_GET['simulacao']);
                     jsonResponse(ConsultaSimulacaoEspecifica('1', $simulacao));
                     break;
+                case 'Consulta_Abc_Plano':
+                    $plano = $_GET['plano'];
+                    jsonResponse(ConsultaAbcPlano('1', $plano));
+                    break;
+                case 'Consulta_Categorias':
+                    jsonResponse(ConsultaCategorias('1'));
+                    break;
                 case 'Consulta_Ultimo_Calculo':
                     $plano = isset($_GET['plano']) ? $_GET['plano'] : null;
                     jsonResponse(Consulta_Ultimo_Calculo('1', $plano));
-                    break;        
+                    break;
                 default:
                     jsonResponse(['status' => false, 'message' => 'Ação GET não reconhecida.']);
                     break;
-
             }
         }
         break;
@@ -63,27 +56,26 @@ switch ($_SERVER["REQUEST_METHOD"]) {
         $dados = $requestData['dados'] ?? null;
         if ($acao) {
             switch ($acao) {
-                case 'CalculoPcs_baseaado_MP':
+                case 'Analise_Materiais':
                     $dadosObjeto = (object) $dados;
                     header('Content-Type: application/json');
                     echo json_encode(AnaliseMateriais('1', $dadosObjeto));
                     break;
-                case 'Analise_Materiais2':
+                case 'Simular_Programacao':
                     $dadosObjeto = (object) $dados;
                     header('Content-Type: application/json');
-                    echo json_encode(AnaliseMateriais2('1', $dadosObjeto));
+                    echo json_encode(Simular_Programacao('1', $dadosObjeto));
                     break;
-                case 'CalculoPcs_baseaado_MP_Simulacao':
+                case 'Cadastro_Simulacao':
                     $dadosObjeto = (object) $dados;
                     header('Content-Type: application/json');
-                    echo json_encode(CalculoPcs_baseaado_MP_Simulacao('1', $dadosObjeto));
+                    echo json_encode(CadastroSimulacao('1', $dadosObjeto));
                     break;
                 case 'detalharSku_x_AnaliseEmpenho':
                     $dadosObjeto = (object) $dados;
                     header('Content-Type: application/json');
                     echo json_encode(detalharSku_x_AnaliseEmpenho('1', $dadosObjeto));
                     break;
-
                 default:
                     jsonResponse(['status' => false, 'message' => 'Ação POST não reconhecida.']);
                     break;
@@ -124,7 +116,7 @@ switch ($_SERVER["REQUEST_METHOD"]) {
 
 function ConsultarPlanos($empresa)
 {
-    $baseUrl = ($empresa == "1") ? 'http://192.168.0.183:9000' : 'http://10.162.0.191:9000';
+    $baseUrl = ($empresa == "1") ? 'http://192.168.0.183:9000' : 'http://192.168.0.183:9000';
     $apiUrl = "{$baseUrl}/pcp/api/Plano";
     $ch = curl_init($apiUrl);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
@@ -144,10 +136,10 @@ function ConsultarPlanos($empresa)
     return json_decode($apiResponse, true);
 }
 
-function ConsultarNaturezas($empresa)
+function Consulta_Ultimo_Calculo($empresa, $plano)
 {
     $baseUrl = ($empresa == "1") ? 'http://192.168.0.183:9000' : 'http://10.162.0.191:9000';
-    $apiUrl = "{$baseUrl}/pcp/api/naturezaEstoqueComponentes";
+    $apiUrl = "{$baseUrl}/pcp/api/obtendoUltimaAnalise_porPlano?codPlano={$plano}";
     $ch = curl_init($apiUrl);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     curl_setopt($ch, CURLOPT_HTTPHEADER, [
@@ -159,183 +151,6 @@ function ConsultarNaturezas($empresa)
 
     if (!$apiResponse) {
         error_log("Erro na requisição: " . curl_error($ch), 0);
-    }
-
-    curl_close($ch);
-
-    return json_decode($apiResponse, true);
-}
-
-function ConsultarComprometidos($empresa)
-{
-    $baseUrl = ($empresa == "1") ? 'http://192.168.0.183:8000' : 'http://10.162.0.191:8000';
-    $apiUrl = "{$baseUrl}/pcp/api/comprometidoOP";
-    $ch = curl_init($apiUrl);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_HTTPHEADER, [
-        'Content-Type: application/json',
-        "Authorization: a44pcp22",
-    ]);
-
-    $apiResponse = curl_exec($ch);
-
-    if (!$apiResponse) {
-        error_log("Erro na requisição: " . curl_error($ch), 0);
-    }
-
-    curl_close($ch);
-
-    return json_decode($apiResponse, true);
-}
-
-function ConsultarComprometidoCompras($empresa)
-{
-    $baseUrl = ($empresa == "1") ? 'http://192.168.0.183:8000' : 'http://10.162.0.191:8000';
-    $apiUrl = "{$baseUrl}/pcp/api/comprometidoCompras";
-    $ch = curl_init($apiUrl);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_HTTPHEADER, [
-        'Content-Type: application/json',
-        "Authorization: a44pcp22",
-    ]);
-
-    $apiResponse = curl_exec($ch);
-
-    if (!$apiResponse) {
-        error_log("Erro na requisição: " . curl_error($ch), 0);
-    }
-
-    curl_close($ch);
-
-    return json_decode($apiResponse, true);
-}
-
-
-function DetalhaNecessidade($empresa, $dados)
-{
-    $baseUrl = ($empresa == "1") ? 'http://192.168.0.183:9000' : 'http://10.162.0.191:9000';
-    $apiUrl = "{$baseUrl}/pcp/api/DetalhaNecessidade";
-
-    $ch = curl_init($apiUrl);
-
-    $options = [
-        CURLOPT_CUSTOMREQUEST => "POST",
-        CURLOPT_POSTFIELDS => json_encode($dados),
-        CURLOPT_RETURNTRANSFER => true,
-        CURLOPT_HTTPHEADER => [
-            'Content-Type: application/json',
-            "Authorization: a44pcp22",
-        ],
-    ];
-
-    curl_setopt_array($ch, $options);
-
-    $apiResponse = curl_exec($ch);
-
-    if (curl_errno($ch)) {
-        $error = curl_error($ch);
-        error_log("Erro na solicitação cURL: {$error}");
-        return false;
-    }
-
-    curl_close($ch);
-
-    return json_decode($apiResponse, true);
-}
-
-
-function AnaliseMateriais($empresa, $dados)
-{
-    $baseUrl = ($empresa == "1") ? 'http://192.168.0.183:9000' : 'http://10.162.0.191:9000';
-    $apiUrl = "{$baseUrl}/pcp/api/CalculoPcs_baseaado_MP";
-
-    $ch = curl_init($apiUrl);
-
-    $options = [
-        CURLOPT_CUSTOMREQUEST => "POST",
-        CURLOPT_POSTFIELDS => json_encode($dados),
-        CURLOPT_RETURNTRANSFER => true,
-        CURLOPT_HTTPHEADER => [
-            'Content-Type: application/json',
-            "Authorization: a44pcp22",
-        ],
-    ];
-
-    curl_setopt_array($ch, $options);
-
-    $apiResponse = curl_exec($ch);
-
-    if (curl_errno($ch)) {
-        $error = curl_error($ch);
-        error_log("Erro na solicitação cURL: {$error}");
-        return false;
-    }
-
-    curl_close($ch);
-
-    return json_decode($apiResponse, true);
-}
-
-function CalculoPcs_baseaado_MP_Simulacao($empresa, $dados)
-{
-    $baseUrl = ($empresa == "1") ? 'http://192.168.0.183:9000' : 'http://10.162.0.191:9000';
-    $apiUrl = "{$baseUrl}/pcp/api/CalculoPcs_baseaado_MP";
-
-    $ch = curl_init($apiUrl);
-
-    $options = [
-        CURLOPT_CUSTOMREQUEST => "POST",
-        CURLOPT_POSTFIELDS => json_encode($dados),
-        CURLOPT_RETURNTRANSFER => true,
-        CURLOPT_HTTPHEADER => [
-            'Content-Type: application/json',
-            "Authorization: a44pcp22",
-        ],
-    ];
-
-    curl_setopt_array($ch, $options);
-
-    $apiResponse = curl_exec($ch);
-
-    if (curl_errno($ch)) {
-        $error = curl_error($ch);
-        error_log("Erro na solicitação cURL: {$error}");
-        return false;
-    }
-
-    curl_close($ch);
-
-    return json_decode($apiResponse, true);
-}
-
-
-
-
-function detalharSku_x_AnaliseEmpenho($empresa, $dados)
-{
-    $baseUrl = ($empresa == "1") ? 'http://192.168.0.183:9000' : 'http://10.162.0.191:9000';
-    $apiUrl = "{$baseUrl}/pcp/api/detalharSku_x_AnaliseEmpenho";
-
-    $ch = curl_init($apiUrl);
-
-    $options = [
-        CURLOPT_CUSTOMREQUEST => "POST",
-        CURLOPT_POSTFIELDS => json_encode($dados),
-        CURLOPT_RETURNTRANSFER => true,
-        CURLOPT_HTTPHEADER => [
-            'Content-Type: application/json',
-            "Authorization: a44pcp22",
-        ],
-    ];
-
-    curl_setopt_array($ch, $options);
-
-    $apiResponse = curl_exec($ch);
-
-    if (curl_errno($ch)) {
-        $error = curl_error($ch);
-        error_log("Erro na solicitação cURL: {$error}");
-        return false;
     }
 
     curl_close($ch);
@@ -347,51 +162,6 @@ function ConsultaSimulacoes($empresa)
 {
     $baseUrl = ($empresa == "1") ? 'http://192.168.0.183:9000' : 'http://192.168.0.183:9000';
     $apiUrl = "{$baseUrl}/pcp/api/ConsultaSimulacoes";
-    $ch = curl_init($apiUrl);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_HTTPHEADER, [
-        'Content-Type: application/json',
-        "Authorization: a44pcp22",
-    ]);
-
-    $apiResponse = curl_exec($ch);
-
-    if (!$apiResponse) {
-        error_log("Erro na requisição: " . curl_error($ch), 0);
-    }
-
-    curl_close($ch);
-
-    return json_decode($apiResponse, true);
-}
-
-
-function ConsultaAbcPlano($empresa, $plano)
-{
-    $baseUrl = ($empresa == "1") ? 'http://192.168.0.183:9000' : 'http://192.168.0.183:9000';
-    $apiUrl = "{$baseUrl}/pcp/api/consultaPlanejamentoABC_plano?codPlano={$plano}";
-    $ch = curl_init($apiUrl);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_HTTPHEADER, [
-        'Content-Type: application/json',
-        "Authorization: a44pcp22",
-    ]);
-
-    $apiResponse = curl_exec($ch);
-
-    if (!$apiResponse) {
-        error_log("Erro na requisição: " . curl_error($ch), 0);
-    }
-
-    curl_close($ch);
-
-    return json_decode($apiResponse, true);
-}
-
-function ConsultaCategorias($empresa)
-{
-    $baseUrl = ($empresa == "1") ? 'http://192.168.0.183:9000' : 'http://192.168.0.183:9000';
-    $apiUrl = "{$baseUrl}/pcp/api/CategoriasDisponiveis";
     $ch = curl_init($apiUrl);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     curl_setopt($ch, CURLOPT_HTTPHEADER, [
@@ -432,10 +202,10 @@ function ConsultaSimulacaoEspecifica($empresa, $simulacao)
     return json_decode($apiResponse, true);
 }
 
-function Consulta_Ultimo_Calculo($empresa, $plano)
+function ConsultaCategorias($empresa)
 {
-    $baseUrl = ($empresa == "1") ? 'http://192.168.0.183:9000' : 'http://10.162.0.191:9000';
-    $apiUrl = "{$baseUrl}/pcp/api/obtendoUltimaAnalise_porPlano?codPlano={$plano}";
+    $baseUrl = ($empresa == "1") ? 'http://192.168.0.183:9000' : 'http://192.168.0.183:9000';
+    $apiUrl = "{$baseUrl}/pcp/api/CategoriasDisponiveis";
     $ch = curl_init($apiUrl);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     curl_setopt($ch, CURLOPT_HTTPHEADER, [
@@ -447,6 +217,62 @@ function Consulta_Ultimo_Calculo($empresa, $plano)
 
     if (!$apiResponse) {
         error_log("Erro na requisição: " . curl_error($ch), 0);
+    }
+
+    curl_close($ch);
+
+    return json_decode($apiResponse, true);
+}
+
+function ConsultaAbcPlano($empresa, $plano)
+{
+    $baseUrl = ($empresa == "1") ? 'http://192.168.0.183:9000' : 'http://192.168.0.183:9000';
+    $apiUrl = "{$baseUrl}/pcp/api/consultaPlanejamentoABC_plano?codPlano={$plano}";
+    $ch = curl_init($apiUrl);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, [
+        'Content-Type: application/json',
+        "Authorization: a44pcp22",
+    ]);
+
+    $apiResponse = curl_exec($ch);
+
+    if (!$apiResponse) {
+        error_log("Erro na requisição: " . curl_error($ch), 0);
+    }
+
+    curl_close($ch);
+
+    return json_decode($apiResponse, true);
+}
+
+
+
+function AnaliseMateriais($empresa, $dados)
+{
+    $baseUrl = ($empresa == "1") ? 'http://192.168.0.183:9000' : 'http://192.168.0.183:9000';
+    $apiUrl = "{$baseUrl}/pcp/api/CalculoPcs_baseaado_MP";
+
+    $ch = curl_init($apiUrl);
+
+    $options = [
+        CURLOPT_CUSTOMREQUEST => "POST",
+        CURLOPT_POSTFIELDS => json_encode($dados),
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_HTTPHEADER => [
+            'Content-Type: application/json',
+            "Authorization: a44pcp22",
+        ],
+    ];
+
+    curl_setopt_array($ch, $options);
+
+    $apiResponse = curl_exec($ch);
+
+    if (curl_errno($ch)) {
+        $error = curl_error($ch);
+        error_log("Erro na solicitação cURL: {$error}");
+        return false;
     }
 
     curl_close($ch);
@@ -493,10 +319,11 @@ function DeleteSimulacao($empresa, $dados)
     return json_encode($response);
 }
 
-function AnaliseMateriais2($empresa, $dados)
+
+function Simular_Programacao($empresa, $dados)
 {
-    $baseUrl = ($empresa == "1") ? 'http://192.168.0.183:9000' : 'http://192.168.0.183:9000';
-    $apiUrl = "{$baseUrl}/pcp/api/AnaliseMateriaisPelaTendencia";
+    $baseUrl = ($empresa == "1") ? '192.168.0.183:9000' : '192.168.0.183:9000';
+    $apiUrl = "{$baseUrl}/pcp/api/CalculoPcs_baseaado_MP";
 
     $ch = curl_init($apiUrl);
 
@@ -522,5 +349,69 @@ function AnaliseMateriais2($empresa, $dados)
 
     curl_close($ch);
 
-    return json_decode($apiResponse,true);
+    return json_decode($apiResponse, true);
+}
+
+function detalharSku_x_AnaliseEmpenho($empresa, $dados)
+{
+    $baseUrl = ($empresa == "1") ? 'http://192.168.0.183:9000' : 'http://10.162.0.191:9000';
+    $apiUrl = "{$baseUrl}/pcp/api/detalharSku_x_AnaliseEmpenho";
+
+    $ch = curl_init($apiUrl);
+
+    $options = [
+        CURLOPT_CUSTOMREQUEST => "POST",
+        CURLOPT_POSTFIELDS => json_encode($dados),
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_HTTPHEADER => [
+            'Content-Type: application/json',
+            "Authorization: a44pcp22",
+        ],
+    ];
+
+    curl_setopt_array($ch, $options);
+
+    $apiResponse = curl_exec($ch);
+
+    if (curl_errno($ch)) {
+        $error = curl_error($ch);
+        error_log("Erro na solicitação cURL: {$error}");
+        return false;
+    }
+
+    curl_close($ch);
+
+    return json_decode($apiResponse, true);
+}
+
+function CadastroSimulacao($empresa, $dados)
+{
+    $baseUrl = ($empresa == "1") ? '192.168.0.183:9000' : '192.168.0.183:9000';
+    $apiUrl = "{$baseUrl}/pcp/api/atualizaInserirSimulacao";
+
+    $ch = curl_init($apiUrl);
+
+    $options = [
+        CURLOPT_CUSTOMREQUEST => "POST",
+        CURLOPT_POSTFIELDS => json_encode($dados),
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_HTTPHEADER => [
+            'Content-Type: application/json',
+            "Authorization: a44pcp22",
+        ],
+    ];
+
+    curl_setopt_array($ch, $options);
+
+    $apiResponse = curl_exec($ch);
+
+    if (curl_errno($ch)) {
+        $error = curl_error($ch);
+        error_log("Erro na solicitação cURL: {$error}");
+        return false;
+    }
+
+    curl_close($ch);
+
+    return json_decode($apiResponse, true);
 }
