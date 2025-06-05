d812ff84154a01d51d6573b9e2a6c33d9bbfc957
Luis Fernando Gonçalves <80552965+luis-fe@users.noreply.github.com>
2025-06-05 09:51:17 -0300

    atualizando

--- nova_versao/Pcp/TendenciaSku/index.php
+++ nova_versao/Pcp/TendenciaSku/index.php
@@ -65,18 +65,18 @@ include_once('../../templates/headerPcp.php');
         <table class="table table-bordered" id="table-tendencia" style="width: 100%;">
             <thead>
                 <tr>
-                    <th>Marca<br><input type="search" class="search-input search-input-tendencia" style="min-width: 100px;"></th>
-                    <th>Referência<br><input type="search" class="search-input search-input-tendencia" style="min-width: 100px;"></th>
+                    <th>Marca<br><input type="search" class="search-input search-input-tendencia" style="min-width: 90px;"></th>
+                    <th>Referência<br><input type="search" class="search-input search-input-tendencia" style="min-width: 90px;"></th>
                     <th>Tamanho<br><input type="search" class="search-input search-input-tendencia"></th>
-                    <th>Cor<br><input type="search" class="search-input search-input-tendencia" style="min-width: 80px;"></th>
+                    <th>Cor<br><input type="search" class="search-input search-input-tendencia" style="min-width: 70px;"></th>
                     <th>Descrição<br><input type="search" class="search-input search-input-tendencia"></th>
-                    <th>Reduzido<br><input type="search" class="search-input search-input-tendencia" style="min-width: 100px;"></th>
+                    <th>Reduzido<br><input type="search" class="search-input search-input-tendencia" style="min-width: 90px;"></th>
                     <th>Categoria<br><input type="search" class="search-input search-input-tendencia" style="min-width: 130px;"></th>
                     <th>Abc<br><input type="search" class="search-input search-input-tendencia" style="min-width: 80px;"></th>
-                    <th>Abc<br>Categoria</br><input type="search" class="search-input search-input-tendencia" style="min-width: 80px;"></th>
+                    <th>Abc Categoria<br><input type="search" class="search-input search-input-tendencia" style="min-width: 80px;"></th>
                     <th>Qtd.de<br>Pedidos</br></th>
                     <th>Valor<br>Vendido</br></th>
-                    <th>Previsão de<br>Vendas</br></th>
+                    <th>Previsão<br>de Vendas</br></th>
                     <th>Qtd. Pedida<br></th>
                     <th>Qtd. Faturada<br></th>
                     <th>Saldo<br>Col Anterior</br></th>
