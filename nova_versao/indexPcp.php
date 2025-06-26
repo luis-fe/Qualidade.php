dfb8e90e8267ee10975bb7ad037fdd7706fda5e6
Luis Fernando Gonçalves <80552965+luis-fe@users.noreply.github.com>
2025-06-26 09:32:40 -0300

    teste

--- nova_versao/indexPcp.php
+++ nova_versao/indexPcp.php
@@ -104,7 +104,7 @@
 </head>
 
 <body>
-    <img src="iconeModuloPCP.png" alt="Tela de Login" class="img-fluid mx-auto d-block mb-4" style="width: 200px; height: auto;">
+    <img src="iconeModuloPCP.png" alt="Tela de Login" class="img-fluid mx-auto d-block mb-4" style="width: 1000px; height: auto;">
 
     <div class="login-box">
         <h2 class="mb-4">Login</h2>
