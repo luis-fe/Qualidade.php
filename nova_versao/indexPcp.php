5628899a713fd0837472a64df4547ffe4fe9038c
Luis Fernando Gonçalves <80552965+luis-fe@users.noreply.github.com>
2025-06-26 09:28:19 -0300

    atualizando

--- nova_versao/indexPcp.php
+++ nova_versao/indexPcp.php
@@ -7,6 +7,7 @@
     <title>Tela de Login</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.0/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <style>
         body {
             background: lightgray;
