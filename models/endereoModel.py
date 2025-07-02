4829b13ed54bd8d91c214ce1092c1c0dff44a146
Luis Fernando Gonçalves <80552965+luis-fe@users.noreply.github.com>
2025-07-02 09:03:57 -0300

    correcao de api para retirar os pedidos fake da analise

--- models/endereoModel.py
+++ models/endereoModel.py
@@ -123,7 +123,7 @@ def ImportEndereco(rua, ruaLimite, modulo, moduloLimite, posicao, posicaoLimite,
                 if imprimir == True:
 
                     #imprimirEtiquetaModel.EtiquetaPrateleira('teste.pdf', codendereco, ruaAtual, moduloAtual, posicaoAtual, natureza)
-                    imprimirEtiquetaModel.gerar_etiquetas_pdf('teste.pdf', [codendereco, ruaAtual, moduloAtual, posicaoAtual, natureza])
+                    imprimirEtiquetaModel.gerar_etiquetas_pdf('teste.pdf',[(codendereco, ruaAtual, moduloAtual, posicaoAtual, natureza)])
 
                     if codempresa == '1':
                         imprimirEtiquetaModel.imprimir_pdf('teste.pdf')
