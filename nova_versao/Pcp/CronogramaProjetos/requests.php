e79b2bda02cbaa55954da60327f488a472c9c867
Luis Fernando Gonçalves <80552965+luis-fe@users.noreply.github.com>
2025-07-15 10:24:26 -0300

    tentativa

--- nova_versao/Pcp/CronogramaProjetos/requests.php
+++ nova_versao/Pcp/CronogramaProjetos/requests.php
@@ -74,7 +74,7 @@ switch ($_SERVER["REQUEST_METHOD"]) {
 
 function ConsultarCronograma()
 {
-    $empresa == "1";
+    $empresa = "1";
     $baseUrl = ($empresa == "1") ? 'http://192.168.0.183:9000' : 'http://10.162.0.191:9000';
     $apiUrl = "{$baseUrl}/pcp/api/obterCronogramaAtividades";
     $ch = curl_init($apiUrl);
