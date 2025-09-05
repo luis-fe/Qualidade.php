406b93713015b5f931b31470ae7b10cbe7367ef1
Luis Fernando Gonçalves <80552965+luis-fe@users.noreply.github.com>
2025-09-05 17:02:14 -0300

    promovendo alteracao

--- nova_versao/Pcp/TendenciaSku/index.php
+++ nova_versao/Pcp/TendenciaSku/index.php
@@ -13,11 +13,13 @@ include_once('../../templates/headerPcp.php');
     /* --- Modais principais (nível base) --- */
     #modal-simulacao,
     #modal-nova-simulacao,
+    #modal-detalhamento-OrdemProd,
     #modal-selecaoEngenharias {
     z-index: 1200 !important;
     }
     #modal-simulacao + .modal-backdrop,
     #modal-nova-simulacao + .modal-backdrop,
+    #modal-detalhamento-OrdemProd + .modal-backdrop,
     #modal-selecaoEngenharias + .modal-backdrop {
     z-index: 1190 !important;
     }
@@ -25,13 +27,11 @@ include_once('../../templates/headerPcp.php');
     /* --- Modais de detalhamento (nível acima) --- */
     #modal-detalhamento-skus,
     #modal-detalhamento-pedidos,
-    #modal-detalhamento-OrdemProd,
     #modal-detalhamento-pedidosSaldo {
     z-index: 1300 !important;
     }
     #modal-detalhamento-skus + .modal-backdrop,
     #modal-detalhamento-pedidos + .modal-backdrop,
-    #modal-detalhamento-OrdemProd + .modal-backdrop,
     #modal-detalhamento-pedidosSaldo + .modal-backdrop {
     z-index: 1290 !important;
     }
