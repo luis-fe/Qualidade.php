7dc9990b359e1dd0e0afc014e797d1545d3c23d9
Luis Fernando Gonçalves <80552965+luis-fe@users.noreply.github.com>
2025-09-06 14:56:59 -0300

    teste

--- nova_versao/Pcp/TendenciaSku/index.php
+++ nova_versao/Pcp/TendenciaSku/index.php
@@ -121,7 +121,7 @@ include_once('../../templates/headerPcp.php');
 
 <div id="simulacao-container" 
      class="mt-3 div-simulacao mx-auto" 
-     style="width: 60%; background-color: lightgray; border-radius: 8px; border: 1px solid black; padding: 16px; position: relative;">
+     style="width: 60%; background-color: lightgray; border-radius: 8px; border: 1px solid black; padding: 16px; position: relative; top: 20px;">
     
     <!-- Botão fechar -->
     <button type="button" onclick="fecharSimulacao()" 
