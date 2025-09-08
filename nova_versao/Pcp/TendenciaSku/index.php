961a7fffbaa1797230aba18e5d95133ce99b52f1
Luis Fernando Gonçalves <80552965+luis-fe@users.noreply.github.com>
2025-09-08 11:23:52 -0300

    atualizando

--- nova_versao/Pcp/TendenciaSku/index.php
+++ nova_versao/Pcp/TendenciaSku/index.php
@@ -118,6 +118,9 @@ include_once('../../templates/headerPcp.php');
 </div>
 
 
+
+
+
 <div 
     id="simulacao-container" 
     class="mt-3 div-simulacao mx-auto d-none p-3 border border-dark rounded" 
@@ -206,7 +209,48 @@ include_once('../../templates/headerPcp.php');
 </div>
 
 
+<div 
+    id="modal-selecaoEngenharias" 
+    class="mt-3 div-selecaoEngenharias mx-auto d-none p-3 border border-dark rounded" 
+    style="width: 60%; max-height: 80vh; overflow-y: auto; background-color: lightgray; position: fixed; top: 20px; left: 50%; transform: translateX(-50%); z-index: 1050;">
+    
+                <div class="modal-header">
+                <h5 class="modal-title" style="color: black;">Seleção de Engenharias</h5>
+                <button type="button" class="btn-close-custom" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body" style=" min-width: 100%; max-height: 600px; overflow: auto">
+                <div class="div-tabela-lotes-csw" style="max-width: 100%; overflow: auto;">
+                    <table class="table table-bordered table-striped" id="table-lotes-csw" style="width: 100%;">
+                        <thead>
+                            <tr>
+                                <th>Marca<br><input type="search" class="search-input search-input-lotes-csw" style="min-width: 70px;"></th>
+                                <th>Código Produto<br><input type="search" class="search-input search-input-lotes-csw" style="min-width: 150px;"></th>
+                                <th>Descrição<br><input type="search" class="search-input search-input-lotes-csw" style="min-width: 150px;"></th>
+                                <th>Percentual<br></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <!-- Aqui vão os dados da tabela -->
+                        </tbody>
+                    </table>
+                </div>
+                <div class="custom-pagination-container pagination-lotes-csw d-md-flex col-12 text-center text-md-start">
+                    <div id="custom-info" class="col-12 col-md-6 mb-2 mb-md-0">
+                        <label for="text">Itens por página</label>
+                        <input id="itens-lotes-csw" class="input-itens" type="text" value="10" min="1">
+                    </div>
+                    <div id="pagination-lotes-csw" class="col-12 col-md-6 d-flex justify-content-center justify-content-md-end">
+                    </div>
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button class="btn btn-salvar" style="width: 100px" id="btn-salvarProdutosSimulacao">
+                    <span><i class="bi bi-floppy"></i></span>
+                    Salvar
+                </button>
+            </div>
 
+</div>
 
 <?php
 include_once('../../templates/footerPcp.php');
