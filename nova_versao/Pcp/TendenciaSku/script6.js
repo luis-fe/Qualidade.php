2c80b3c9feaf27563d12aba92634e14b547ce162
Luis Fernando Gonçalves <80552965+luis-fe@users.noreply.github.com>
2025-09-08 11:51:39 -0300

    atulizando teste

--- nova_versao/Pcp/TendenciaSku/script6.js
+++ nova_versao/Pcp/TendenciaSku/script6.js
@@ -702,6 +702,107 @@ async function Produtos_Simulacao() {
 }
 
 
+async function Detalha_OrdemProducao(codReduzido) {
+            $('#loadingModal').modal('show');
+
+    try {
+        const response = await $.ajax({
+            type: 'GET',
+            url: 'requests.php',
+            dataType: 'json',
+            data: {
+                acao: "Detalha_OrdemProducao",
+                codReduzido
+                    }
+        });
+        console.log(response)
+        TabelaDetalhamentoOrdemProd(response);
+        const modal = new bootstrap.Modal(document.getElementById('modal-detalhamento-OrdemProd'));
+        modal.show();
+    } catch (error) {
+        console.error('Erro ao consultar ordemProd:', error);
+    }finally {
+            $('#loadingModal').modal('hide');
+        }
+};
+
+
+
+function TabelaDetalhamentoOrdemProd(listaDetalhes) {
+    if ($.fn.DataTable.isDataTable('#table-detalhamento-OrdemProd')) {
+        $('#table-detalhamento-OrdemProd').DataTable().destroy();
+    }
+
+    const tabela = $('#table-detalhamento-OrdemProd').DataTable({
+        searching: true,
+        paging: true,
+        lengthChange: false,
+        info: false,
+        pageLength: 15,
+        dom: 'Bfrtip', // <-- necessário para os botões aparecerem
+        buttons: [
+            {
+                extend: 'excelHtml5',
+                text: '<i class="bi bi-file-earmark-spreadsheet-fill"></i> Excel',
+                title: 'Tendências de Vendas',
+                className: 'btn-tabelas',
+                exportOptions: {
+                    columns: ':visible',
+                    format: {
+                        body: function (data, row, column, node) {
+                            if (typeof data === 'string') {
+                                return data.replace(/\./g, '').replace(',', '.');
+                            }
+                            return data;
+                        }
+                    }
+                }
+            }
+        ],
+        data: listaDetalhes,
+        columns: [
+            { data: 'numeroop' },
+            {data: 'codFaseAtual'},
+            {data: 'nomeFase'},
+            { data: 'total_pcs' },
+        ],
+        language: {
+            paginate: {
+                previous: '<i class="fa-solid fa-backward-step"></i>',
+                next: '<i class="fa-solid fa-forward-step"></i>'
+            },
+            info: "Página _PAGE_ de _PAGES_",
+            emptyTable: "Nenhum dado disponível na tabela",
+            zeroRecords: "Nenhum registro encontrado"
+        },
+        drawCallback: function () {
+            $('#pagination-detalhamento-ordemProd').html($('.dataTables_paginate').html());
+            $('#pagination-detalhamento-ordemProd span').remove();
+            $('#pagination-detalhamento-ordemProd a').off('click').on('click', function (e) {
+                e.preventDefault();
+                if ($(this).hasClass('previous')) tabela.page('previous').draw('page');
+                if ($(this).hasClass('next')) tabela.page('next').draw('page');
+            });
+            $('.dataTables_paginate').hide();
+        }
+    });
+
+    // Adiciona os botões à interface
+    tabela.buttons().container().appendTo('#table-detalhamento-ordemProd .col-md-6:eq(0)');
+
+    $('#itens-detalhamento-ordemProd').on('input', function () {
+        const valor = parseInt($(this).val(), 10);
+        if (!isNaN(valor) && valor > 0) {
+            tabela.page.len(valor).draw();
+        }
+    });
+
+    $('.search-input-detalhamento-ordemProd').on('input', function () {
+        tabela.column($(this).closest('th').index()).search($(this).val()).draw();
+    });
+}
+
+
 
 function TabelaEngenharia(lista) {
     if ($.fn.DataTable.isDataTable('#table-lotes-csw')) {
