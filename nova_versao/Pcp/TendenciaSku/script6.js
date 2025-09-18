2d5eda091b0923888d059970639ada5d04275aac
Luis Fernando Gonçalves <80552965+luis-fe@users.noreply.github.com>
2025-09-18 12:40:23 -0300

    atualizando

--- nova_versao/Pcp/TendenciaSku/script6.js
+++ nova_versao/Pcp/TendenciaSku/script6.js
@@ -982,6 +982,7 @@ async function Simular_Programacao(simulacao, tipo) {
         respostaPeriodoVendas.inicioFaturamento = formatarDataBrasileira(respostaPeriodoVendas.inicioFaturamento);
         respostaPeriodoVendas.finalFaturamento = formatarDataBrasileira(respostaPeriodoVendas.finalFaturamento);
         respostaPeriodoVendas.metaPcs = formatarInteiro(respostaPeriodoVendas.metaPcs);
+        respostaPeriodoVendas.metaFinanceira = formatarMoedaBrasileira(respostaPeriodoVendas.metaFinanceira);
 
 
         $('#titulo').html(`
@@ -1010,9 +1011,20 @@ async function Simular_Programacao(simulacao, tipo) {
     <!-- Novo Card -->
     <div class="card border rounded me-1" style="width: 190px;">
       <div class="card-body p-0">
-        <h5 class="card-title bg-primary text-white p-0 m-0 text-center">Meta R$</h5>
+            <h5 class="card-title bg-primary text-white p-0 m-0 text-center">Meta R$</h5>
+            <p class="card-text m-0">
+            <strong>${respostaPeriodoVendas.metaFinanceira}</strong>
+            </p>
+        </div>
+    </div>
+
+  
+    </div>
+    <div class="card border rounded me-1" style="width: 190px;">
+      <div class="card-body p-0">
+        <h5 class="card-title bg-primary text-white p-0 m-0 text-center">Meta Pçs</h5>
         <p class="card-text m-0">
-          <strong>${respostaPeriodoVendas.metaFinanceira}</strong>
+          <strong>${respostaPeriodoVendas.metaPcs}</strong>
         </p>
       </div>
     </div>
@@ -1026,6 +1038,7 @@ async function Simular_Programacao(simulacao, tipo) {
 </div>
 
           `);
+
 
     $('#btn-informacoes').on('click', function () {
     
