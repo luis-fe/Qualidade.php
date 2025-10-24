f7d8de58ac8b00f6b61e82c6b122622fbc612c7b
Luis Fernando Gonçalves <80552965+luis-fe@users.noreply.github.com>
2025-10-24 15:37:43 -0300

    atualiza

--- nova_versao/Qualidade/Inicio/index.php
+++ nova_versao/Qualidade/Inicio/index.php
@@ -32,7 +32,6 @@ include_once('../../templates/headerGarantia.php');
     flex: 1 1 45%;
     min-width: 50px;
     background: #fff;
-    padding: 5px;
     border-radius: 10px;
     box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
   }
