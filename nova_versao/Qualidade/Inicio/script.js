d980f0f49b8321f333407166142220f9395b0e4d
Luis Fernando Gonçalves <80552965+luis-fe@users.noreply.github.com>
2025-10-24 16:11:13 -0300

    atualzia

--- nova_versao/Qualidade/Inicio/script.js
+++ nova_versao/Qualidade/Inicio/script.js
@@ -213,12 +213,12 @@ const renderizarGrafico = (pecasComMotivo, totalPecasBaixadas) => {
 };
 
 async function renderizarGraficoBarras(data) {
-    const chartWidth = Math.max(500, data.length * 50);
+    const chartWidth = Math.max(400, data.length * 50);
 
     const chartOptions = {
         chart: {
             type: 'bar',
-            height: 500,
+            height: 400,
             width: `${chartWidth}px`,  // Mantém a largura dinâmica
             toolbar: { show: false },
             dropShadow: { enabled: false }
