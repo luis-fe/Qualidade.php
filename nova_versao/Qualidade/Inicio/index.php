8bf18064c205a00f2abf3966eff236455b8cf859
Luis Fernando Gonçalves <80552965+luis-fe@users.noreply.github.com>
2025-10-24 12:43:49 -0300

    atualizando

--- nova_versao/Qualidade/Inicio/index.php
+++ nova_versao/Qualidade/Inicio/index.php
@@ -96,8 +96,8 @@ include_once('../../templates/headerGarantia.php');
 
 <!-- ==================== GRÁFICOS ==================== -->
 <div class="col-12 mt-2">
-  <div class="col-12 mt-1 p-1 grafico-container" 
-  style="max-height: 300px; overflow-y: auto;">
+  <div class="col-12 mt-1 p-0 grafico-container" 
+        style="max-height: 300px; overflow-y: auto;">
 
     <div class="grafico card mb-2" style="width: 100%;">
         <div class="card-header p-2">
