afde9fdd6b88eaa92bcadc99fc859bfa86fbadf5
luis.fernando <luiis_engp@hotmail.com>
2025-12-03 17:04:30 -0300

    atualizando o processo de requisicao

---
+++ Gestao/nova_versao/Procedimentos Industriais/index.php
@@ -0,0 +1,51 @@
+<?php
+include_once("../../../templates/LoadingGestao.php");
+include_once('../../../templates/headerGestao.php');
+?>
+<!-- Adicione o CSS do Select2 -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.13/css/select2.min.css">
+<link rel="stylesheet" href="style.css">
+<style>
+</style>
+
+<div class="titulo-tela">
+    <span class="span-icone"><i class="bi bi-stack"></i></span> Procedimentos Industriais
+</div>
+
+<div class="col-12  bg-light p-0 border-start "> 
+    <a href="URL_DESTINO_DO_ANUNCIO" target="_blank">
+        <img src="Diagrama em branco.jpeg" class="img-fluid rounded shadow-sm" alt="Descrição do Anúncio" style="width: 2080px; height: 900px;">
+    </a>
+</div>
+
+<div class="col-12 bg-light p-0 border-start d-flex">
+    <div class="col-md-8 p-0">
+                <h5 class="mb-2">Rotina de Geração de Requisicao Independente - CCSERQ010</h5>
+        <a href="URL_DESTINO_DO_ANUNCIO" target="_blank">
+            <img src="CSW_MODELOREQjpg.jpg"
+                 class="img-fluid rounded shadow-sm"
+                 alt="Descrição do Anúncio"
+                 style="height: 600px;">
+        </a>
+    </div>
+
+    <div class="col-md-4 p-3 d-flex flex-column justify-content-center">
+        <p class="mb-0"><strong>Responsabilidade:</strong> Setor de Expedicao de Terceirizados, </br>gera a requisicao de transferencia
+        se atentando ao tipo: Transferencia, nat.Origem 3 , nat.Destino 12 e observação com o nome do terceirizado
+    </p>
+    </div>
+</div>
+
+
+
+<?php
+include_once('../../../templates/footerGestao.php');
+?>
+<script src="script1.js"></script>
+
+<script>
+    // Se o seu código de controle de visibilidade estiver aqui,
+    // ele funcionará perfeitamente, pois o $ já estará disponível.
+    
+
+</script>
