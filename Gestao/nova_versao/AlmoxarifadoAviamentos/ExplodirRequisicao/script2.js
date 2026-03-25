24c603bbaf50ba46123dbec7a84013759253c222
luis-fe <luis_engp@hotmail.com>
2026-03-25 09:00:48 -0300

    ajustando a etiqueta do separador

--- Gestao/nova_versao/AlmoxarifadoAviamentos/ExplodirRequisicao/script2.js
+++ Gestao/nova_versao/AlmoxarifadoAviamentos/ExplodirRequisicao/script2.js
@@ -56,10 +56,10 @@ const cardHTML = `
                 
                 <div style="height: 0.25cm;"></div>
 
-                <div class="d-flex justify-content-between align-items-center" style="font-size: 1.0rem; flex: 1;">
-                    <strong>OP: <strong>${item.numOPConfec}</strong></strong>
-                    <strong>${tagSeparador}</strong>
-                    <strong>Qtd: <strong style="font-size: 1.5rem;">${item.qtdeRequisitada}</strong></strong>
+                <div class="d-flex align-items-center" style="font-size: 1.0rem; flex: 1; gap: 0;">
+                        <strong>OP: <strong style="font-size: 1.2rem;">${item.numOPConfec}</strong></strong>
+                        <strong>${tagSeparador}</strong>
+                        <strong>Qtd: <strong style="font-size: 1.5rem;">${item.qtdeRequisitada}</strong></strong>
                 </div>
             </div>
 
